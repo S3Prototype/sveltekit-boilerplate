3dfb95b737d833b434b5401d512799f93bdae7b5
S3Prototype <liuqahs15@gmail.com>
2023-06-21 08:41:36 -0500

    Testing isomorphic functions

--- api/url-example.ts
+++ api/url-example.ts
@@ -1,9 +1,8 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(request: VercelRequest, response: VercelResponse) {
+export default async function handler(request, response) {
 	if (!request.url) return response.status(400);
 
-	const { searchParams } = new URL(request.url);
+	const url = new URL(request.url, `http://${request.headers.host}`);
+	const { searchParams } = url;
 	const hasTitle = searchParams.has('title');
 	const title = hasTitle ? searchParams.get('title')?.slice(0, 100) : 'My default title';
 
